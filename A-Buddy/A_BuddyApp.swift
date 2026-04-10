a6d2ec70291c453ac6044aa62b9e2c237ebe3ff7
KrisH <baguskrishnanditya@gmail.com>
2026-04-10 08:37:09 +0700

    Initial Commit

---
+++ A-Buddy/A_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_BuddyApp.swift
+//  A-Buddy
+//
+//  Created by Bagus Krishna on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct A_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
